94615e43396e6dfc2091ac7ca8d4de36bd4dd04a
Leo Trubach <leotrubach@gmail.com>
2020-11-15 07:02:14 +0600

    Deprecation fixes

--- jet/tests/urls.py
+++ jet/tests/urls.py
@@ -9,17 +9,17 @@ try:
     from django.urls import path
 
     urlpatterns = [
-        url(r'^jet/', include('jet.urls', 'jet')),
-        url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
-        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+        re_path(r'^jet/', include('jet.urls', 'jet')),
+        re_path(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
+        re_path(r'^admin/doc/', include('django.contrib.admindocs.urls')),
         path('admin/', admin.site.urls),
     ]
 except ImportError:  # Django < 2.0
     urlpatterns = [
-        url(r'^jet/', include('jet.urls', 'jet')),
-        url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
-        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-        url(r'^admin/', include(admin.site.urls)),
+        re_path(r'^jet/', include('jet.urls', 'jet')),
+        re_path(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
+        re_path(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+        re_path(r'^admin/', include(admin.site.urls)),
     ]
 
 if django.VERSION[:2] < (1, 8):
